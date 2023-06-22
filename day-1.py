d858e79df09469114d85369bc01c14df102f228e
Charlie Stanard <charlie.stanard@guildeducation.com>
2023-06-21 19:16:34 -0500

    day 1, part 1 solution

---
+++ day-1.py
@@ -0,0 +1,17 @@
+input = open('day-1-input.txt')
+
+data = input.read().splitlines()
+index = 0
+results = []
+for item in data:
+    if item:
+        if len(results) > index:
+            value = results[index]
+            results[index] = value + int(item)
+        else:
+            results.insert(index, int(item))
+    else:
+        index += 1
+
+print(max(results))
+input.close()
